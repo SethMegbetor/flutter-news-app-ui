98bb3a810496de8072d8d5acaa08440fbf1f6e09
SethMegbetor <sethmegbetor@gmail.com>
2023-03-13 20:55:11 +0000

    added bottom navigation bar and hid the labels

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,33 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black.withAlpha(100),
+        items: [
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+              label: "Search"),
+          BottomNavigationBarItem(
+              icon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.person),
+              ),
+              label: "Profile"),
+        ],
+      ),
       body: Container(),
     );
   }

c19a063f17cf3f51fead8e76bf73262de7c2d9f6
SethMegbetor <sethmegbetor@gmail.com>
2023-03-14 09:28:12 +0000

    Done with search bar for the discover screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/bottom_nav_bar.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
@@ -7,9 +9,39 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("No Profile"),
+    // return const Scaffold(
+    //   body: Center(
+    //     child: Text("No Profile"),
+    //   ),
+    // );
+        return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      bottomNavigationBar: const BottomNavBar(index: 2),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Column(
+            children: [
+              Text(
+                "Profile",
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.bold),
+              )
+            ],
+          )
+        ],
       ),
     );
   }

e5e1a12ed05fb3e0427a62c4f8cf7b02d73c0588
SethMegbetor <sethmegbetor@gmail.com>
2023-03-14 10:55:24 +0000

    done with the discover screen

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_ui/screens/screens.dart';
+import 'package:news_app_ui/widgets/image_container.dart';
 
+import '../models/article_model.dart';
 import '../widgets/bottom_nav_bar.dart';
 
 class DiscoverScreen extends StatelessWidget {
@@ -9,23 +12,147 @@ class DiscoverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.black,
+    List<String> tabs = [
+      "Health",
+      "Politics",
+      "Art",
+      "Food",
+      "Science",
+    ];
+
+    return DefaultTabController(
+      initialIndex: 0,
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
           ),
         ),
+        bottomNavigationBar: const BottomNavBar(index: 1),
+        body: ListView(
+          padding: const EdgeInsets.all(20.0),
+          children: [const _DiscoverNews(), _CategoryNews(tabs: tabs)],
+        ),
       ),
-      bottomNavigationBar: const BottomNavBar(index: 1),
-      body: ListView(
-        padding: const EdgeInsets.all(20.0),
-        children: const [_DiscoverNews()],
-      ),
+    );
+  }
+}
+
+class _CategoryNews extends StatelessWidget {
+  const _CategoryNews({
+    super.key,
+    required this.tabs,
+  });
+
+  final List<String> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    final articles = Article.articles;
+
+    return Column(
+      children: [
+        TabBar(
+          isScrollable: true,
+          indicatorColor: Colors.black,
+          tabs: tabs
+              .map(
+                (tab) => Tab(
+                  icon: Text(
+                    tab,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: TabBarView(
+              children: tabs
+                  .map(
+                    (tab) => ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: articles.length,
+                      itemBuilder: ((context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(
+                              context,
+                              ArticleScreen.routeName,
+                              arguments: articles[index],
+                            );
+                          },
+                          child: Row(
+                            children: [
+                              ImageContainer(
+                                width: 80,
+                                height: 80,
+                                margin: const EdgeInsets.all(10.0),
+                                borderRadius: 5,
+                                imageUrl: articles[index].imageUrl,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      articles[index].title,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.clip,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyLarge!
+                                          .copyWith(
+                                              fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.schedule,
+                                          size: 18,
+                                        ),
+                                        const SizedBox(width: 5),
+                                        Text(
+                                          "${DateTime.now().difference(articles[index].createdAt).inHours} hours ago",
+                                          style: const TextStyle(fontSize: 12),
+                                        ),
+                                        const SizedBox(width: 20),
+                                        const Icon(
+                                          Icons.visibility,
+                                          size: 18,
+                                        ),
+                                        const SizedBox(width: 5),
+                                        Text(
+                                          "${articles[index].views} views",
+                                          style: const TextStyle(fontSize: 12),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
+                  )
+                  .toList()),
+        )
+      ],
     );
   }
 }

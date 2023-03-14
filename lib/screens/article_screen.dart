b244696c27e5216bde310d9b657a2dbc0a6a3a58
SethMegbetor <sethmegbetor@gmail.com>
2023-03-14 12:37:01 +0000

    done with tut

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_ui/models/article_model.dart';
+import 'package:news_app_ui/widgets/image_container.dart';
+
+import '../widgets/custom_tag.dart';
 
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
@@ -7,6 +11,194 @@ class ArticleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final article = ModalRoute.of(context)!.settings.arguments as Article;
+
+    return ImageContainer(
+      width: double.infinity,
+      imageUrl: article.imageUrl,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          children: [
+            _NewsHeadline(
+              article: article,
+            ),
+            _NewsBody(article: article)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NewsBody extends StatelessWidget {
+  const _NewsBody({
+    super.key,
+    required this.article,
+  });
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CustomTag(
+                backgroundColor: Colors.black,
+                children: [
+                  CircleAvatar(
+                    radius: 10,
+                    backgroundImage: NetworkImage(
+                      article.authorImageUrl,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    article.author,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(color: Colors.white),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 10),
+              CustomTag(
+                backgroundColor: Colors.grey.shade200,
+                children: [
+                  const Icon(
+                    Icons.timer,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                      "${DateTime.now().difference(article.createdAt).inHours} h",
+                      style: Theme.of(context).textTheme.bodyMedium!),
+                ],
+              ),
+              const SizedBox(width: 10),
+              CustomTag(
+                backgroundColor: Colors.grey.shade200,
+                children: [
+                  const Icon(
+                    Icons.remove_red_eye,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(width: 10),
+                  Text("${article.views} h",
+                      style: Theme.of(context).textTheme.bodyMedium!),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Text(
+            article.title,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            article.body,
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(height: 1.5),
+          ),
+          const SizedBox(height: 20),
+          GridView.builder(
+              shrinkWrap: true,
+              itemCount: 2,
+              gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.25,
+              ),
+              itemBuilder: (context, index) {
+                return ImageContainer(
+                  width: MediaQuery.of(context).size.width * 0.42,
+                  imageUrl: article.imageUrl,
+                  margin: const EdgeInsets.only(
+                    right: 5.0,
+                    bottom: 5.0,
+                  ),
+                );
+              })
+        ],
+      ),
+    );
+  }
+}
+
+class _NewsHeadline extends StatelessWidget {
+  const _NewsHeadline({
+    super.key,
+    required this.article,
+  });
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+          ),
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
+            children: [
+              Text(
+                article.category,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.white),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            article.title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  height: 1.25,
+                ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            article.title,
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
   }
 }
